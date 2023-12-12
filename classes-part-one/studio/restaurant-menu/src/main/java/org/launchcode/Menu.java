e83d6a2ba643f8704381e7d8a9298d95e3d37213
JAlycee <124473879+JAlycee@users.noreply.github.com>
2023-12-11 19:29:26 -0600

    Final Commit Classes Part 1

---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -0,0 +1,30 @@
+package org.launchcode;
+import java.util.Date;
+import java.util.ArrayList;
+public class Menu {
+    private Date lastUpdated;
+    private ArrayList<MenuItem> menuItems;
+
+    public Menu() {
+        this.menuItems = new ArrayList<>();
+        this.lastUpdated = new Date(); // Initialize with the current date and time
+    }
+    public void addItem(MenuItem item) {
+        menuItems.add(item);
+        lastUpdated = new Date();
+    }
+
+    public void removeItem(MenuItem item) {
+        menuItems.remove(item);
+        lastUpdated = new Date();
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public ArrayList getMenuItems() {
+        return menuItems;
+    }
+
+};
